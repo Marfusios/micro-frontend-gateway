59237631b45981eb057e90adfa633d5735c74dfd
marekhoryna <marek.horyna@outlook.com>
2019-02-12 15:26:30 +0100

    updated systemjs, added tachyons cdn

--- src/main.ts
+++ src/main.ts
@@ -14,12 +14,15 @@ import * as rxjs from 'rxjs';
 import * as rxjsOperators from 'rxjs/operators';
 import * as tslib from 'tslib';
 import * as aElements from '@angular/elements';
+import * as aPlatformBrowser from '@angular/platform-browser';
+import * as aPlatformBrowserAnimations from '@angular/platform-browser/animations';
 
 
 /**
  * Setup SystemJS modules for remote module dependencies
  */
 declare const SystemJS;
+
 SystemJS.set('@angular/core', SystemJS.newModule(core));
 SystemJS.set('@angular/common', SystemJS.newModule(common));
 SystemJS.set('@angular/common/http', SystemJS.newModule(commonHttp));
@@ -27,6 +30,8 @@ SystemJS.set('@angular/router', SystemJS.newModule(router));
 SystemJS.set('@angular/forms', SystemJS.newModule(forms));
 SystemJS.set('@angular/animations', SystemJS.newModule(animations));
 SystemJS.set('@angular/elements', SystemJS.newModule(aElements));
+SystemJS.set('@angular/platform-browser', SystemJS.newModule(aPlatformBrowser));
+SystemJS.set('@angular/platform-browser/animations', SystemJS.newModule(aPlatformBrowserAnimations));
 SystemJS.set('rxjs', SystemJS.newModule(rxjs));
 SystemJS.set('rxjs/operators', SystemJS.newModule(rxjsOperators));
 SystemJS.set('tslib', SystemJS.newModule(tslib));
